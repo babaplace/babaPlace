b8c4a39067121487c74b89f19a976fbef574c131
Salim <sidymamadousalim@gmail.com>
2024-03-09 15:59:10 +0100

    add meta data

--- src/config/site/siteConfig.ts
+++ src/config/site/siteConfig.ts
@@ -1,7 +1,8 @@
 export const SITECONFIG = {
   logo: "/logo.png",
   seo: {
-    name: "BabaPlace",
-    description: "",
+    name: "BabaPlace - appartement à louer ",
+    description:
+      "Découvrez un large choix de maisons à louer pour vos vacances,votre année étudiante ou déplacements professionnels. Facile, fiable, et sécurisé.",
   },
 };
