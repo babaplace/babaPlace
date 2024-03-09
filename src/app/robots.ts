2960c510e72a9c44ce7229e09ae61960e21262ef
Salim <sidymamadousalim@gmail.com>
2024-03-09 15:56:42 +0100

    add rotbot

---
+++ src/app/robots.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "Googlebot",
+        allow: ["/", "/biens"],
+        disallow: "/private/",
+      },
+      {
+        userAgent: ["Applebot", "Bingbot"],
+        disallow: ["/", "/biens"],
+      },
+    ],
+    sitemap: "https://babaplace.ma/sitemap.xml",
+  };
+}
