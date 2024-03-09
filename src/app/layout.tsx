e69d432e4bb047d98dd8d0c7fbcc72149ca9c630
Salim <sidymamadousalim@gmail.com>
2024-03-09 15:02:23 +0100

    add analytis google 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${"${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"});
+              gtag('config', 'G-QK7YEQ2XV0');
           `}
         </Script>
       </head>
