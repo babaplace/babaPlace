2b0497b408eb613e4374c4a10e282d5d24bedf03
Salim <sidymamadousalim@gmail.com>
2024-03-16 17:37:09 +0000

    add logique insertion

--- src/app/partners/addproperty/step/form/Successful.tsx
+++ src/app/partners/addproperty/step/form/Successful.tsx
@@ -4,13 +4,18 @@ import checkmark from "@/assets/images/icon-checkmark.svg";
 const Successful = () => {
   return (
     <div className="h-full flex flex-col justify-center items-center gap-4">
-      <Image src={checkmark} alt="checkmark" width={100} height={100} className="h-16 w-16" />
+      <Image
+        src={checkmark}
+        alt="checkmark"
+        width={100}
+        height={100}
+        className="h-16 w-16"
+      />
       <div className="flex flex-col items-center gap-1">
-        <h1 className="text-black text-2xl font-bold">Thank you!</h1>
+        <h1 className="text-black text-2xl font-bold"> Merci!</h1>
         <p className="text-gray-500text-xs text-center">
-          Thanks for confirming your suscription! We hope you have fun using our
-          platform. If you ever need support, please feel free to email us at
-          support@loremgaming.com.
+          Merci pour la confiance . Nous allons verifier et valider le produit
+          le plus vite possible
         </p>
       </div>
     </div>
