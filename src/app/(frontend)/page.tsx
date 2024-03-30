01d9dd323a707e789c96af54ccd6fa1a1a5d3d3d
Salim <sidymamadousalim@gmail.com>
2024-03-30 14:58:52 +0000

    add section

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/ui/components/Hero";
 import AllProductsA from "@/ui/components/allProductsA";
 import LessCostProperties from "@/ui/components/LessCostProperty";
-import { AvisSection } from "@/ui/components/Avis";
 import { prisma } from "@/db/prisma";
 import Services from "@/ui/components/Services";
 
